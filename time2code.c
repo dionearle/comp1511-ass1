e1232f1acdb8e7d133cb7a01646306b4fb526826
Dion Earle <z5205292@unsw.edu.au>
2018-04-01 15:59:17 +1000

    give on Sun  1 Apr 15:59:17 AEST 2018

--- time2code.c
+++ time2code.c
@@ -50,8 +50,27 @@
 
 
 // ADD YOUR #defines (if any) here
+
+//All used when the local time has minutes greater than 60 (as this is invalid)
 #define UTC_TIME_LIMIT 2360
 #define MAX_HOURS 2400
+#define MAX_MINUTES 60
+#define OVERFLOW_NEW_HOUR 40
+
+//Months in which daylight savings starts and ends
+#define START_DST_MTH 10
+#define END_DST_MTH 3
+
+//Days on which daylight savings starts and ends
+#define START_DST_DAY 6
+#define END_DST_DAY 31
+//Times at which daylight savings starts and ends for certain timezones
+#define ACDT_SWITCH_HR 1630
+#define AEDT_SWITCH_HR 1600
+#define LHDT_START_HR 1500
+#define LHDT_END_HR 1530
+#define NZDT_SWITCH_HR 1400
+
 
 int get_local_time(int town, int utc_month, int utc_day, int utc_time);
 void run_unit_tests(void);
@@ -62,7 +81,7 @@ int is_valid_town(int town);
 int is_valid_date(int utc_month, int utc_day);
 int is_valid_time(int utc_time);
 int choose_timezone(int town, int utc_month, int utc_day, int utc_time);
-int final_output_checks(int local_time);
+int final_output_checks(int town, int local_time);
 
 
 // DO NOT CHANGE THIS FUNCTION
@@ -148,13 +167,10 @@ int main(void) {
 //
 
 int get_local_time(int town, int utc_month, int utc_day, int utc_time) {
-    // CHANGE THIS FUNCTION
-    // YOU ARE NOT PERMITTED TO USE ARRAYS, LOOPS, PRINTF OR SCANF
-    // SEE THE ASSIGNMENT SPECIFICATION FOR MORE RESTRICTIONS
 
 	int local_time;
 	
-	//Returning Invalid Input if something wrong is entered, else determining timezone
+	//Returns INVALID_INPUT if user input is wrong, else determines timezone
 	if (is_valid_town(town) == INVALID_INPUT) {
 		local_time = INVALID_INPUT;
 	} else if (is_valid_date(utc_month, utc_day) == INVALID_INPUT) {
@@ -165,19 +181,12 @@ int get_local_time(int town, int utc_month, int utc_day, int utc_time) {
 		local_time = choose_timezone(town, utc_month, utc_day, utc_time);
 	}
 
-    //Checks to see if the local time to be outputted is correct. (w/ case for Eucla)
-    //TEST IF SIMILAR THING NEEDS TO BE DONE FOR LORD_HOWE AND CENTRAL_TIME
-    //Since their shift ends in 30 (whilst Eucla ends in 45)
-	if ((local_time >= MAX_HOURS) && (town == TOWN_EUCLA)) {
-        local_time = (local_time - UTC_TIME_LIMIT);
-    } else {
-        local_time = final_output_checks(local_time);
-    }
+
+	//Checks to see if the local time outputted is correct
+    local_time = final_output_checks(town, local_time);
 
 	return local_time;
 }
-
-// ADD YOUR FUNCTIONS HERE
 
 int is_valid_town(int town) {
 
@@ -229,7 +238,7 @@ int is_valid_time(int utc_time) {
 	//Basic test to ensure valid utc time is chosen
 	if ((utc_time < 0) || (utc_time >= UTC_TIME_LIMIT)) {
 		output_time = INVALID_INPUT;
-	} else if ((utc_time%100) >= 60) {
+	} else if ((utc_time % 100) >= MAX_MINUTES) {
 		output_time = INVALID_INPUT;
 	} else {
 		output_time = 0;
@@ -244,14 +253,15 @@ int choose_timezone(int town, int utc_month, int utc_day, int utc_time) {
 
 	//Returning timezone for Adelaide & Broken Hill
 	if ((town == TOWN_ADELAIDE) || (town == TOWN_BROKEN_HILL)) {
-		if ((utc_month < 3) || (utc_month > 10)) {
+		if ((utc_month < END_DST_MTH) || (utc_month > START_DST_MTH)) {
 		    output_timezone = utc_time + TIMEZONE_ACDT_OFFSET;
-		} else if (((utc_month == 3) && (utc_day <= 31))
-		|| ((utc_month == 10) && (utc_day >= 6))) {
-		    output_timezone = utc_time + TIMEZONE_ACDT_OFFSET;       
-		} else if (((utc_month == 3) && (utc_day == 31) && (utc_time < 1630))
-		|| ((utc_month == 10) && (utc_day == 6) && (utc_time >= 1630))) {
-		    output_timezone = utc_time + TIMEZONE_ACDT_OFFSET;       
+		} else if (((utc_month == END_DST_MTH) && (utc_day < END_DST_DAY))
+		    || ((utc_month == START_DST_MTH) && (utc_day > START_DST_DAY))) {
+		        output_timezone = utc_time + TIMEZONE_ACDT_OFFSET;       
+		} else if (((utc_month == END_DST_MTH) && (utc_day == END_DST_DAY) 
+		    && (utc_time < ACDT_SWITCH_HR)) || ((utc_month == START_DST_MTH) 
+		    && (utc_day == START_DST_DAY) && (utc_time >= ACDT_SWITCH_HR))) {
+		        output_timezone = utc_time + TIMEZONE_ACDT_OFFSET;       
 		} else {
 		    output_timezone = utc_time + TIMEZONE_ACST_OFFSET; 
 		}
@@ -265,14 +275,15 @@ int choose_timezone(int town, int utc_month, int utc_day, int utc_time) {
 	//Returning timezone for Sydney, Canberrra, Hobart & Melbourne
 	if ((town == TOWN_SYDNEY) || (town == TOWN_CANBERRA)
 	|| (town == TOWN_HOBART) || (town == TOWN_MELBOURNE)) { 
-		if ((utc_month < 3) || (utc_month > 10)) {
+		if ((utc_month < END_DST_MTH) || (utc_month > START_DST_MTH)) {
 		    output_timezone = utc_time + TIMEZONE_AEDT_OFFSET;
-		} else if (((utc_month == 3) && (utc_day <= 31))
-		|| ((utc_month == 10) && (utc_day >= 6))) {
-		    output_timezone = utc_time + TIMEZONE_AEDT_OFFSET;
-		} else if (((utc_month == 3) && (utc_day == 31) && (utc_time < 1600))
-		|| ((utc_month == 10) && (utc_day == 6) && (utc_time >= 1600))) {
-			output_timezone = utc_time + TIMEZONE_AEDT_OFFSET;
+		} else if (((utc_month == END_DST_MTH) && (utc_day < END_DST_DAY))
+		    || ((utc_month == START_DST_MTH) && (utc_day > START_DST_DAY))) {
+		        output_timezone = utc_time + TIMEZONE_AEDT_OFFSET;
+		} else if (((utc_month == END_DST_MTH) && (utc_day == END_DST_DAY) 
+		    && (utc_time < AEDT_SWITCH_HR)) || ((utc_month == START_DST_MTH) 
+		    && (utc_day == START_DST_DAY) && (utc_time >= AEDT_SWITCH_HR))) {
+			    output_timezone = utc_time + TIMEZONE_AEDT_OFFSET;
 		} else {
 			output_timezone = utc_time + TIMEZONE_AEST_OFFSET; 
 		}
@@ -290,14 +301,15 @@ int choose_timezone(int town, int utc_month, int utc_day, int utc_time) {
 
 	//Returning timezone for Lord Howe Island
 	if (town == TOWN_LORD_HOWE_IS) {
-		if ((utc_month < 3) || (utc_month > 10)) {
+		if ((utc_month < END_DST_MTH) || (utc_month > START_DST_MTH)) {
 		    output_timezone = utc_time + TIMEZONE_LHDT_OFFSET;
-		} else if(((utc_month == 3) && (utc_day <= 31))
-		|| ((utc_month == 10) && (utc_day >= 6))) {
-			output_timezone = utc_time + TIMEZONE_LHDT_OFFSET;
-		} else if(((utc_month == 3) && (utc_day == 31) && (utc_time < 1500))
-		|| ((utc_month == 10) && (utc_day == 6) && (utc_time >= 1530))) {
-			output_timezone = utc_time + TIMEZONE_LHDT_OFFSET;
+		} else if(((utc_month == END_DST_MTH) && (utc_day < END_DST_DAY))
+		    || ((utc_month == START_DST_MTH) && (utc_day > START_DST_DAY))) {
+			    output_timezone = utc_time + TIMEZONE_LHDT_OFFSET;
+		} else if(((utc_month == END_DST_MTH) && (utc_day == END_DST_DAY) 
+		    && (utc_time < LHDT_START_HR)) || ((utc_month == START_DST_MTH) 
+		    && (utc_day == START_DST_DAY) && (utc_time >= LHDT_END_HR))) {
+			    output_timezone = utc_time + TIMEZONE_LHDT_OFFSET;
 		} else {
 			output_timezone = utc_time + TIMEZONE_LHST_OFFSET;
 		}
@@ -311,14 +323,15 @@ int choose_timezone(int town, int utc_month, int utc_day, int utc_time) {
 	//Returning timezone for Auckland, Christchurch & Wellington
 	if (town == TOWN_AUCKLAND  || town == TOWN_CHRISTCHURCH 
 	|| town == TOWN_WELLINGTON) {
-		if ((utc_month < 3) || (utc_month > 10)) {
+		if ((utc_month < END_DST_MTH) || (utc_month > START_DST_MTH)) {
 		    output_timezone = utc_time + TIMEZONE_NZDT_OFFSET;
-		} else if (((utc_month == 3) && (utc_day <= 31))
-		|| ((utc_month == 10) && (utc_day >= 6))) {
-		    output_timezone = utc_time + TIMEZONE_NZDT_OFFSET;
-		} else if (((utc_month == 3) && (utc_day == 31) && (utc_time < 1400))
-		|| ((utc_month == 10) && (utc_day == 6) && (utc_time >= 1400))) {
-			output_timezone = utc_time + TIMEZONE_NZDT_OFFSET;
+		} else if (((utc_month == END_DST_MTH) && (utc_day < END_DST_DAY))
+		    || ((utc_month == START_DST_MTH) && (utc_day > START_DST_DAY))) {
+		        output_timezone = utc_time + TIMEZONE_NZDT_OFFSET;
+		} else if (((utc_month == END_DST_MTH) && (utc_day == END_DST_DAY) 
+		    && (utc_time < NZDT_SWITCH_HR)) || ((utc_month == START_DST_MTH) 
+		    && (utc_day == START_DST_DAY) && (utc_time >= NZDT_SWITCH_HR))) {
+			    output_timezone = utc_time + TIMEZONE_NZDT_OFFSET;
 		} else {
 			output_timezone = utc_time + TIMEZONE_NZST_OFFSET;
 		}
@@ -328,22 +341,29 @@ int choose_timezone(int town, int utc_month, int utc_day, int utc_time) {
 }
 
 
-int final_output_checks(int local_time) {
+int final_output_checks(int town, int local_time) {
 
-    //Changing the minutes if they are greater than 60
-	if ((local_time % 100) >= 60) {
-		local_time = local_time + 40;
+    //Changing the minutes if they end in a number greater than 60
+	if ((local_time % 100) >= MAX_MINUTES) {
+		local_time = local_time + OVERFLOW_NEW_HOUR;
 	}
         
-    //Changing local time if reaches the 24th hour of a day (MAX_HOURS)
+    //Changing local time if reaches the 24th hour of a day
     if (local_time >= MAX_HOURS) {
+        if (town == TOWN_EUCLA) {
+            local_time = (local_time - UTC_TIME_LIMIT);
+    } else
         local_time = (local_time - MAX_HOURS);
     }
 	
 	return local_time;
 }
 
-// ADD A COMMENT HERE EXPLAINING YOUR OVERALL TESTING STRATEGY
+/* To begin, I firstly went through all of the possible cases where the program should return INVALID_INPUT. These were scenarios were a number was entered that did not represent one of the possible towns, months, days, or times. When doing so, I made sure to test close to these boundaries to ensure nothing slipped past.
+
+Next, I ran a simple test for each of the timezones, making sure they all worked under regular conditions. This included both the standard times and daylight savings times, choosing times in the year when these were active. This also ensured I didn't accidentally miss anything.
+
+Finally, the most crucial tests were on the edges of when daylight savings would start and end. I ran four tests for each timezone, testing right before it switched over and as soon as it did switch over for both occurunces in 2018. The many different time changes took quite some time to get my head around, yet it was essential I used these final tests to pick up any miscalculations. */
 
 // run unit tests for get_local_time
 
@@ -356,8 +376,151 @@ void run_unit_tests(void) {
     // 42 is not a valid month
     assert(get_local_time(TOWN_SYDNEY, 42, 0, 0) == INVALID_INPUT);
 
-    // ADD YOUR ASSERT STATEMENTS HERE
+   
+   
+    //Set of tests to ensure the user input is valid
 
+	// -1 is not a valid town
+	assert(get_local_time(-1, 1, 1, 0) == INVALID_INPUT);
+
+	// 14 is not a valid town
+	assert(get_local_time(14, 1, 1, 0) == INVALID_INPUT);
+
+	// 0 is not a valid month
+	assert(get_local_time(TOWN_SYDNEY, 0, 1, 0) == INVALID_INPUT);
+
+	// 13 is not a valid month
+	assert(get_local_time(TOWN_SYDNEY, 13, 1, 0) == INVALID_INPUT);
+
+	// 0 is not a valid day
+	assert(get_local_time(TOWN_SYDNEY, 1, 0, 0) == INVALID_INPUT);
+
+	// 32 is not a valid day
+	assert(get_local_time(TOWN_SYDNEY, 1, 32, 0) == INVALID_INPUT);
+
+	// February 29th is not a valid date
+	assert(get_local_time(TOWN_SYDNEY, 2, 29, 0) == INVALID_INPUT);
+
+	// April 31st is not a valid date
+	assert(get_local_time(TOWN_SYDNEY, 4, 31, 0) == INVALID_INPUT);
+
+	// -1 is not a valid time
+	assert(get_local_time(TOWN_SYDNEY, 1, 1, -1) == INVALID_INPUT);
+
+	// 2360 is not a valid time
+	assert(get_local_time(TOWN_SYDNEY, 1, 1, 2360) == INVALID_INPUT);
+
+	// 2400 is not a valid time
+	assert(get_local_time(TOWN_SYDNEY, 1, 1, 2400) == INVALID_INPUT);
+
+	// 1860 is not a valid time
+	assert(get_local_time(TOWN_SYDNEY, 1, 1, 1860) == INVALID_INPUT);
+
+
+
+	//Testing each timezone works
+
+	// Australian Western Standard Time
+	assert(get_local_time(TOWN_PERTH, 3, 15, 1130) == 1930);
+
+	// Australian Central Western Standard Time
+	assert(get_local_time(TOWN_EUCLA, 8, 22, 815) == 1700);
+
+	// Australian Central Standard Time
+	assert(get_local_time(TOWN_ADELAIDE, 5, 9, 1700) == 230);
+
+	// Australian Central Daylight Time
+	assert(get_local_time(TOWN_BROKEN_HILL, 2, 1, 1400) == 30);
+
+	// Australian Eastern Standard Time
+	assert(get_local_time(TOWN_HOBART, 7, 7, 1930) == 530);
+
+	// Australian Eastern Daylight Time
+	assert(get_local_time(TOWN_MELBOURNE, 11, 26, 2300) == 1000);
+
+	// Lord Howe Standard Time
+	assert(get_local_time(TOWN_LORD_HOWE_IS, 4, 16, 1000) == 2030);
+
+	// Lord Howe Daylight Time
+	assert(get_local_time(TOWN_LORD_HOWE_IS, 12, 29, 1500) == 200);
+
+	// NZ Standard Time
+	assert(get_local_time(TOWN_WELLINGTON, 7, 13, 1900) == 700);
+
+	// NZ Daylight Time
+	assert(get_local_time(TOWN_AUCKLAND, 1, 22, 630) == 1930);
+	
+	// Extra test for Brisbane 
+	assert(get_local_time(TOWN_BRISBANE, 7, 14, 45) == 1045);
+
+	// Extra test for Darwin
+	assert(get_local_time(TOWN_EUCLA, 12, 31, 730) == 1700);
+
+	// Extra test for Perth
+	assert(get_local_time(TOWN_EUCLA, 8, 1, 2320) == 720);
+	
+	
+	
+    //Testing edge cases for when daylight savings starts and ends
+
+
+	// Eastern Timezones
+	
+	// Before AEST Begins
+	assert(get_local_time(TOWN_CANBERRA, 3, 31, 1559) == 259);
+
+	// After AEST Begins
+	assert(get_local_time(TOWN_SYDNEY, 3, 31, 1600) == 200);
+
+	// Before AEDT Begins
+	assert(get_local_time(TOWN_MELBOURNE, 10, 6, 1559) == 159);
+
+	// After AEDT Begins
+	assert(get_local_time(TOWN_HOBART, 10, 6, 1600) == 300);
+
+	//Lord Howe Island Timezones
+
+	// Before LHST Begins
+	assert(get_local_time(TOWN_LORD_HOWE_IS, 3, 31, 1459) == 159);
+
+	// After LHST Begins
+	assert(get_local_time(TOWN_LORD_HOWE_IS, 3, 31, 1500) == 130);
+
+	// Before LHDT Begins
+	assert(get_local_time(TOWN_LORD_HOWE_IS, 10, 6, 1529) == 159);
+
+	// After LHDT Begins
+	assert(get_local_time(TOWN_LORD_HOWE_IS, 10, 6, 1530) == 230);
+
+	//Central Timezones
+
+	// Before ACST Begins
+	assert(get_local_time(TOWN_ADELAIDE, 3, 31, 1629) == 259);
+
+	// After ACST Begins
+	assert(get_local_time(TOWN_BROKEN_HILL, 3, 31, 1630) == 200);
+
+	// Before ACDT Begins
+	assert(get_local_time(TOWN_BROKEN_HILL, 10, 6, 1629) == 159);
+
+	// After ACDT Begins
+	assert(get_local_time(TOWN_ADELAIDE, 10, 6, 1630) == 300);
+
+	//New Zealand Timezones
+
+	// Before NZST Begins
+	assert(get_local_time(TOWN_AUCKLAND, 3, 31, 1359) == 259);
+
+	// After NZST Begins
+	assert(get_local_time(TOWN_WELLINGTON, 3, 31, 1400) == 200);
+
+	// Before NZDT Begins
+	assert(get_local_time(TOWN_AUCKLAND, 10, 6, 1359) == 159);
+
+	// After NZDT Begins
+	assert(get_local_time(TOWN_CHRISTCHURCH, 10, 6, 1400) == 300);
+	
+	
     // you should add at least 40 more assert statements to this function
     // with a comment for each test explaining it
 
